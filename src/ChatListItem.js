6e823c0a84b1ab1bdb76ea2659fbb7daaa91bc1e
thomasroest <user@example.com>
2018-08-26 20:28:32 +0200

    chat list updates

--- src/ChatListItem.js
+++ src/ChatListItem.js
@@ -3,34 +3,49 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledListItem = styled.li`
+const StyledListItem = styled.a`
   background: white;
-  padding: 1em;
+
   border-bottom: 1px solid lightgrey;
   display: flex;
-  justify-content: space-between;
   &:active {
     background-color: lightgray;
   }
 `;
 
-const ListItemImage = styled.div`
-  img {
-    border-radius: 50%;
+const ChatAvatar = styled.div`
+  flex-basis: 15%;
+  border: 1px solid blue;
+  background-image: url("https://www.placecage.com/50/50");
+`;
+
+const ChatTitle = styled.div`
+  padding: 10px;
+  flex-basis: 70%;
+  border: 1px solid green;
+  font-weight: bold;
+  span {
+    color: grey;
+    font-size: 0.8em;
   }
+`;
+
+const ChatMeta = styled.div`
+  flex-basis: 15%;
+  border: 1px solid purple;
+  flex-grow: 1;
 `;
 
 const ChatListItem = () => {
   return (
-    <a href="">
-      <StyledListItem>
-        <ListItemImage>
-          <img src="https://www.placecage.com/50/50" alt="" />
-        </ListItemImage>
-        <section>Username and info here some other stuff</section>
-        <section>meta</section>
-      </StyledListItem>
-    </a>
+    <StyledListItem>
+      <ChatAvatar />
+      <ChatTitle>
+        This is the chat title <br />
+        <span>preview text here</span>
+      </ChatTitle>
+      <ChatMeta>meta data</ChatMeta>
+    </StyledListItem>
   );
 };
 
