7b9d369ed4e11e444053fe0c97b609ce07299196
thomasroest <user@example.com>
2018-08-28 15:33:09 +0200

    update list item styling

--- src/ChatListItem.js
+++ src/ChatListItem.js
@@ -6,11 +6,11 @@ import styled from "styled-components";
 const StyledListItem = styled.a`
   background: white;
   padding: 10px;
-  /* border-bottom: 1px solid lightgrey; */
+  border-bottom: 1px solid #e4e4e4;
   display: flex;
   align-items: center;
   &:active {
-    background-color: lightgray;
+    background-color: #e4e4e4;
   }
 `;
 
@@ -27,14 +27,13 @@ const ChatTitle = styled.div`
   padding-left: 10px; /* padding: 15px 10px 10px 10px; */
   flex-basis: 70%;
   font-size: 1.1em;
-  /* border-bottom: 0.5px solid #e4e4e4; */
-  /* border: 1px solid green; */
   h3 {
     font-size: 1em;
     font-weight: 700;
     margin: 0;
   }
   span {
+    /* margin: 100px; */
     color: grey;
     font-size: 0.9em;
   }
@@ -44,6 +43,7 @@ const ChatMeta = styled.div`
   /* padding: 15px 10px 10px 10px; */
   flex-basis: 15%;
   color: grey;
+  font-size: 0.7em;
   /* border: 1px solid purple; */
   flex-grow: 1;
   /* align-self: flex-start; */
@@ -59,7 +59,7 @@ const ChatListItem = () => {
         <h3>This is the chat title</h3>
         <span>preview text here</span>
       </ChatTitle>
-      <ChatMeta>meta</ChatMeta>
+      <ChatMeta>06-08-18</ChatMeta>
     </StyledListItem>
   );
 };
